ff9e00cd74d6215f2ccfdf33f09adc9a51ac6211
MHafizAF <hafiz170301@gmail.com>
2022-09-17 17:28:25 +0700

    disable the line break style

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-underscore-dangle */
 class NotesHandler {
   constructor(service) {
--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const routes = require('./routes');
 const NotesHandler = require('./handler');
 

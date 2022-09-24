8d95f9719510e3feb87b28ad79d6d6608ee3a2d5
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-09-24 17:46:22 +0700

    final fix

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
-const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
   const notesService = new NotesService();
-
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -4,7 +4,9 @@ const { NotePayloadSchema } = require('./schema');
 const NotesValidator = {
   validateNotePayload: (payload) => {
     const validationResult = NotePayloadSchema.validate(payload);
-    if (validationResult.error) throw new InvariantError(validationResult.error.message);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
   },
 };
 
